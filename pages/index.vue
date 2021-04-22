e35a00c5b7c768c7b19a46a6c4b470d2c117b949
vydrova <v.janca@gmail.com>
2021-04-22 20:41:58 +0200

    homepage code

--- pages/index.vue
+++ pages/index.vue
@@ -16,8 +16,10 @@
           <a href="/">
             <div class="product-box">
               <img src="https://static.wixstatic.com/media/74f558_993f041afc4045c69c824d6022d78ca0~mv2_d_4000_2667_s_4_2.jpeg/v1/fill/w_702,h_726,fp_0.50_0.50,q_85,usm_0.66_1.00_0.01/74f558_993f041afc4045c69c824d6022d78ca0~mv2_d_4000_2667_s_4_2.webp">
-              <h3>Obklady a dlažby</h3>
-              <div class="product-box__text">Krátký popis co se v tomto bylíčku nabízí</div>
+              <div class="product-box__text">
+                <h3>Obklady a dlažby</h3>
+                <p>Krátký popis co se v tomto bylíčku nabízí</p>
+              </div>
             </div>
           </a>
         </div>
@@ -25,8 +27,10 @@
           <a href="/">
             <div class="product-box">
               <img src="https://static.wixstatic.com/media/74f558_4ef3f401e4a2400a8979c42d1a7ba1a7~mv2_d_5760_3840_s_4_2.jpeg/v1/fill/w_693,h_726,fp_0.50_0.50,q_85,usm_0.66_1.00_0.01/74f558_4ef3f401e4a2400a8979c42d1a7ba1a7~mv2_d_5760_3840_s_4_2.webp">
-              <h3>Okna a dveře</h3>
-              <div class="product-box__text">Krátký popis co se v tomto bylíčku nabízí</div>
+              <div class="product-box__text">
+                <h3>Okna a dveře</h3>
+                <p>Krátký popis co se v tomto bylíčku nabízí</p>
+              </div>
             </div>
           </a>
         </div>
@@ -34,8 +38,10 @@
           <a href="/">
             <div class="product-box">
               <img src="https://static.wixstatic.com/media/74f558_9b1cc2c7424d47bf83b890c15ed8127f~mv2_d_5625_3750_s_4_2.jpg/v1/fill/w_693,h_726,fp_0.50_0.50,q_85,usm_0.66_1.00_0.01/74f558_9b1cc2c7424d47bf83b890c15ed8127f~mv2_d_5625_3750_s_4_2.webp">
-              <h3>Ploty a zámková dlažba</h3>
-              <div class="product-box__text">Krátký popis co se v tomto bylíčku nabízí</div>
+              <div class="product-box__text">
+                <h3>Ploty a zámková dlažba</h3>
+                <p>Krátký popis co se v tomto bylíčku nabízí</p>
+              </div>
             </div>
           </a>
         </div>
